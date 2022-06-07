f7bcc9bafc13b252bc22881d977f07b0353946e3
Yury Philipov <yury.philipov@gmail.com>
2022-06-07 15:50:14 +0800

    Gulp template v.0.2

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const gcmq = require('gulp-group-css-media-queries');
 const cleancss = require('gulp-clean-css');
 const uglify = require('gulp-uglify-es').default;
 const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
 const browserSync = require('browser-sync').create();
 
 function browsersync() {
@@ -17,7 +18,7 @@ function browsersync() {
 
 function styles() {
   return gulp
-    .src('app/scss/**/*.scss')
+    .src(['!app/scss/**/_*.scss', 'app/scss/**/*.scss'])
     .pipe(sass())
     .pipe(gcmq())
     .pipe(concat('app.min.css'))
@@ -32,21 +33,50 @@ function styles() {
 
 function scripts() {
   return gulp
-    .src('app/js/**/*.js')
+    .src([
+      'node_modules/jquery/dist/jquery.min.js',
+      'app/js/**/*.js',
+      '!app/js/**/*.min.js'
+    ])
     .pipe(concat('app.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('app/js'))
     .pipe(browserSync.reload({ stream: true }));
 }
 
+function images() {
+  return gulp
+    .src('app/img/src/**/*')
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }]
+        })
+      ])
+    )
+    .pipe(gulp.dest('app/img/'))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
 function startwatch() {
   gulp.watch('app/scss/**/*.scss', styles);
   gulp.watch(['app/js/**/*.js', '!app/js/**/*.min.js'], scripts);
+  gulp.watch('app/img/src/**/*', images);
   gulp.watch('app/*.html').on('change', browserSync.reload);
 }
 
 exports.browsersync = browsersync;
 exports.styles = styles;
 exports.scripts = scripts;
+exports.images = images;
 
-exports.default = gulp.parallel(scripts, styles, browsersync, startwatch);
+exports.default = gulp.parallel(
+  scripts,
+  styles,
+  images,
+  browsersync,
+  startwatch
+);
